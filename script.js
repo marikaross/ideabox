62c79355fedb0ea1133842a3d9de13fdc95b9962
Tory Dannenberg <rfdberg@gmail.com>
2018-04-04 14:30:45 -0600

    Refactor some JS and psuedo code edit content function

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-$ideaTitleField = $('.idea-title');
-$ideaBodyField = $('.idea-body');
-$saveButton = $('.save-button');
-$deleteButton = $('.delete');
-// $ideaQuality = $('.quality');
-// $upVoteButton = $('.up-vote');
-// $downVoteButton = $('.down-vote');
-var $ideaContainer = $('.idea-container')
+var $ideaTitleField = $('.idea-title');
+var $ideaBodyField = $('.idea-body');
+var $saveButton = $('.save-button');
+var $deleteButton = $('.delete');
+var $ideaContainer = $('.idea-container');
 var ideaTitle = $ideaTitleField.val();
 var ideaBody = $ideaBodyField.val();
-// var newestIdea = new CreateIdea(ideaTitle, ideaBody);
 
 $saveButton.on('click', generateIdea);
 $('section').on('click', '.delete-icon', deleteIdea);
 $('section').on('click', '.up-vote', increaseQuality);
 $('section').on('click', '.down-vote', decreaseQuality);
+$('.idea-container').on('keydown blur', 'p',  saveEdits);
+$('.idea-container').on('click', 'p',  allowEdits);
 
-$(document).ready(function() {
-  // console.log(localStorage);
+$(document).ready(retrieveSavedIdeas); 
+
+function allowEdits() {
+  $(this).attr('contentEditable', true);
+}
+
+function retrieveSavedIdeas() {
   for (var i = 0; i < localStorage.length; i++) {
     var storedIdea = JSON.parse(localStorage.getItem(localStorage.key(i)));
-
-    console.log(storedIdea)
     prependIdeas(storedIdea);
-  }
-})
+  };
+};
 
 function CreateIdea(title, body) {
   this.title = title;
@@ -37,7 +38,6 @@ function generateIdea(e) {
   var ideaTitle = $ideaTitleField.val();
   var ideaBody = $ideaBodyField.val();
   var newestIdea = new CreateIdea(ideaTitle, ideaBody);
-  console.log(newestIdea)
   prependIdeas(newestIdea);
   sendIdeatoStorage(newestIdea);
 };
@@ -52,37 +52,21 @@ function retrieveIdeaFromStorage(id) {
   var retrievedIdea = localStorage.getItem(id);
 };
 
-// CreateIdea.prototype.prependCards = function(idea) {
-//   $('.idea-container').prepend(`
-//     <article id="${idea.id}">
-//       <button class="delete-icon"></button>
-//       <h2>${idea.title}</h2>
-//       <p contenteditable="true">${idea.body}</p>
-//       <button class="up-vote"></button>
-//       <button class="down-vote"></button>
-//       <p class="rating">quality:<span class="quality">swill</span></p>
-//       <hr>
-//     </article>
-//     `);
-//   clearInputFields();
-//   $ideaTitleField.focus()
-// };
 
 function prependIdeas (idea) {
-  // console.log('p')
   $('.idea-container').prepend(`
     <article id="${idea.id}">
-      <button class="delete-icon"></button>
-      <h2>${idea.title}</h2>
-      <p contenteditable="true">${idea.body}</p>
-      <button class="up-vote"></button>
-      <button class="down-vote"></button>
-      <p class="rating">quality:<span class="quality">swill</span></p>
-      <hr>
+    <button class="delete-icon"></button>
+    <h2>${idea.title}</h2>
+    <p contenteditable="true">${idea.body}</p>
+    <button class="up-vote"></button>
+    <button class="down-vote"></button>
+    <p class="rating">quality:<span class="quality">swill</span></p>
+    <hr>
     </article>
     `);
   clearInputFields();
-  $ideaTitleField.focus()
+  $ideaTitleField.focus();
 };
 
 function clearInputFields() {
@@ -92,25 +76,23 @@ function clearInputFields() {
 
 function deleteIdea() {
   $(this).closest('article').remove();
-  localStorage.removeItem($(this).closest('article'));
+  localStorage.removeItem($(this).closest('article').attr('id'));
 };
 
 function increaseQuality() {
   var $currentQuality = $(this).siblings('p').children('.quality').text();
   var newQuality = '';
-    if ($currentQuality === 'swill') {
-      newQuality = 'plausible';
-    } else if ($currentQuality === 'plausible') {
-      newQuality = 'genius';
-    } else {
-      return;
-    };
-    var currentCardId = $(this).closest('article').attr('id');
-    $(this).siblings('p').children('.quality').text(newQuality);
-    console.log(newQuality, currentCardId);
+  if ($currentQuality === 'swill') {
+    newQuality = 'plausible';
+  } else if ($currentQuality === 'plausible') {
+    newQuality = 'genius';
+  } else {
+    return;
   };
+  var currentCardId = $(this).closest('article').attr('id');
+  $(this).siblings('p').children('.quality').text(newQuality);
+};
 
-// Re-factor decreaseQuality same as above
 function decreaseQuality() {
   var $currentQuality = $(this).siblings('p').children('.quality').text();
   var newQuality = '';
@@ -123,5 +105,25 @@ function decreaseQuality() {
   };
   $(this).siblings('p').children('.quality').text(newQuality);
 };
+
+function saveEdits(e) {
+    var currentCard = $(this).closest('article');
+  if (e.keyCode === 13) {
+    sendIdeatoStorage(currentCard);
+    $(this).attr('contentEditable', false);
+  };
+};
+
+
+
+// Save edits to variable 
+// Use key of this article to retrieve from local storage
+// Parse
+// Replace body using save edits variable
+// Stringify
+// Send back to storage
+
+
+
 
 
